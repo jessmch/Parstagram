3d5a9114ecb8154016c0f733652839a5fb1a642d
jessmch <minhchau241102@gmail.com>
2021-05-03 20:00:24 +0700

    Parstagram App Creation

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "mTcF0PTGqqZO2YrA3ibrNHGVozvqWblU0BjZoCMT" // <- UPDATE
+                    $0.clientKey = "sVQYQOjjiRDSlQOvx88mL26R6d5XRwr6rzy2uw7f" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+            
+        // --- end copy
+
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,23 @@
+platform :ios, '9.0'
+
+target 'Parstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Parstagram
+  pod 'Parse'
+  pod 'AlamofireImage'
+
+  # Pods for YOUR_APP
+  pod 'Parse'
+
+  target 'ParstagramTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ParstagramUITests' do
+    # Pods for testing
+  end
+
+end
